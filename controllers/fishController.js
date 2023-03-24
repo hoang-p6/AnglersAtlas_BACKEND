63209816d732c9d293eafc58ed717e8dbd0c216a
Phong Hoang <p.hoang6900@gmail.com>
2023-03-24 16:52:30 -0400

    Created fish controllers

---
+++ controllers/fishController.js
@@ -0,0 +1,62 @@
+const Fish = require('../models/lfish')
+
+const getAllFish = async (req, res) => {
+  try {
+    const lfish = await Fish.find()
+    return res.status(200).json({ lfish })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+const getFishById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const fish = await Fish.findById(id)
+    if (fish) {
+      return res.status(200).json({ fish })
+    }
+    return res.status(404).send('Requested fish does not exist')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+const createFish = async (req, res) => {
+  try {
+    const fish = await new Fish(req.body)
+    await fish.save()
+    return res.status(201).json({
+      lure
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+const deleteFish = async (req, res) => {
+  try {
+    const { id } = req.params
+    const deleted = await Fish.findByIdAndDelete(id)
+    if (deleted) {
+      return res.status(200).send('Fish deleted')
+    }
+    throw new Error('Fish not found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+const updateFish = async (req, res) => {
+  try {
+    const fish = await Fish.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+    res.status(200).json(lure)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+module.exports = {
+  getAllFish,
+  getFishById,
+  deleteFish,
+  updateFish,
+  createFish
+}
--- controllers/lureController.js
+++ controllers/lureController.js
@@ -15,7 +15,7 @@ const getLureById = async (req, res) => {
     if (lure) {
       return res.status(200).json({ lure })
     }
-    return res.status(404).send('Requested user does not exist')
+    return res.status(404).send('Requested lure does not exist')
   } catch (error) {
     return res.status(500).send(error.message)
   }
