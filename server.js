971eae157b0c123ef9cdf9819ae51800796e9548
Phong Hoang <p.hoang6900@gmail.com>
2023-04-28 12:00:30 -0400

    Editted CORS

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
 app.use(cors())
+app.get('/products/:id', function (req, res, next) {
+  res.json({ msg: 'This is CORS-enabled for all origins!' })
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 app.use('/api', routes)
 app.use('/auth', AuthRouter)
 
