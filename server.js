64a42c403a2a124095495ec014d680a562339d8d
Phong Hoang <p.hoang6900@gmail.com>
2023-04-28 22:21:30 -0400

    Updated cors to allow frontend access

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const { Water } = require('./models/water')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
-app.use(cors())
+app.use(
+  cors({
+    origin: 'https://anglersatlas.surge.sh'
+  })
+)
 app.get('/products/:id', function (req, res, next) {
   res.json({ msg: 'This is CORS-enabled for all origins!' })
 })
