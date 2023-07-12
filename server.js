a489992d08e7eb6e4a4713acee2b4128d28aacac
Phong Hoang <p.hoang6900@gmail.com>
2023-07-12 18:53:23 -0400

    Changed CORS header

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(cors())
 // })
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://anglersatlas.surge.sh/')
+  req.header('Access-Control-Allow-Origin', 'https://anglersatlas.surge.sh/')
   next()
 })
 
