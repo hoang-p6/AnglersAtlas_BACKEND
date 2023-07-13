bd3caa7ee80e53c1f98a77b198f3127a559fc59b
Phong Hoang <p.hoang6900@gmail.com>
2023-07-12 21:19:08 -0400

    Comment out line 18 in server.js; debugging

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(logger('dev'))
 app.use(cors())
 
-app.use('/api', routes)
+// app.use('/api', routes)
 app.use('/', routes)
 app.use('/auth', AuthRouter)
 
