8d5a7f6bdb2d44e887ad223378e52109915f2688
Phong Hoang <p.hoang6900@gmail.com>
2023-07-12 21:11:25 -0400

    Working on adding route for /

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(logger('dev'))
 app.use(cors())
 
 app.use('/api', routes)
+app.use('/', routes)
 app.use('/auth', AuthRouter)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error'))
